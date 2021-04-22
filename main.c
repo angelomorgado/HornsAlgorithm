0246371a3188906d551a0c740f31e8fff1e82976
angelomorgado <angelo_morgado@hotmail.com>
2021-04-22 21:29:34 +0100

    Added a bunch of functions

--- main.c
+++ main.c
@@ -11,7 +11,19 @@ typedef struct Expressao{
     int tamNegativos;
 }Expressao;
 
-//
+//Verifica se um char c existe num qualquer array de chars lista
+int existeNaLista(char *lista, char c, int tam)
+{
+    for(int i=0; i<tam; i++)
+    {
+        if(lista[i] == c)
+            return 1;
+    }
+    return 0;
+}
+
+
+//Divide a linha em expressoes e separa os valores negativos e positivos na estrutura Expressao
 int DesfragmentarLinha(char *linha, Expressao **lista)
 {
     //indice que vamos utilizar para percorrer a lista
@@ -38,9 +50,12 @@ int DesfragmentarLinha(char *linha, Expressao **lista)
         if(linha[i] == '~')
         {
             //Atualiza a (*lista) de negativos e adiciona o caracter negativo
-            (*lista)[tamLista-1].tamNegativos++;
-            (*lista)[tamLista-1].Negativos = realloc((*lista)[tamLista-1].Negativos, (*lista)[tamLista-1].tamNegativos * sizeof(char));
-            (*lista)[tamLista-1].Negativos[(*lista)[tamLista-1].tamNegativos-1] = linha[i+1];
+            if(existeNaLista((*lista)[tamLista-1].Negativos, linha[i+1], (*lista)[tamLista-1].tamNegativos) == 0)
+            {
+                (*lista)[tamLista-1].tamNegativos++;
+                (*lista)[tamLista-1].Negativos = realloc((*lista)[tamLista-1].Negativos, (*lista)[tamLista-1].tamNegativos * sizeof(char));
+                (*lista)[tamLista-1].Negativos[(*lista)[tamLista-1].tamNegativos-1] = linha[i+1];
+            }
             //Avança na (*lista) para não repetir o caracter
             i++;
         }
@@ -48,16 +63,30 @@ int DesfragmentarLinha(char *linha, Expressao **lista)
         //Se for uma letra, adicionar aos positivos
         if(((linha[i] >= 65 && linha[i] <=90) || (linha[i] >= 97 && linha[i] <=122)) && linha[i-1] != '~')
         {
-            //Atualiza a (*lista) de positivos e adiciona o caracter positivo
-            (*lista)[tamLista-1].tamPositivos++;
-            (*lista)[tamLista-1].Positivos = realloc((*lista)[tamLista-1].Positivos, (*lista)[tamLista-1].tamPositivos * sizeof(char));
-            (*lista)[tamLista-1].Positivos[(*lista)[tamLista-1].tamPositivos-1] = linha[i];
+            if(existeNaLista((*lista)[tamLista-1].Positivos, linha[i], (*lista)[tamLista-1].tamPositivos) == 0)
+            {
+                //Atualiza a (*lista) de positivos e adiciona o caracter positivo
+                (*lista)[tamLista-1].tamPositivos++;
+                (*lista)[tamLista-1].Positivos = realloc((*lista)[tamLista-1].Positivos, (*lista)[tamLista-1].tamPositivos * sizeof(char));
+                (*lista)[tamLista-1].Positivos[(*lista)[tamLista-1].tamPositivos-1] = linha[i];
+            }
         }
 
         //Avançar na linha
         i++;
     }
     return tamLista;
+}
+
+//Dá reset á lista de expressões para que possa ser utilizada novamente devolve o valor do tamanho da lista como 0
+int reset(Expressao **lista)
+{
+    //Liberta a memória em heap da lista
+    free(*lista);
+    //Volta a inicializar a lista
+    *lista = malloc(1);
+
+    return 0;
 }
 
 void debugLinha(Expressao *lista, int tamLista)
@@ -80,6 +109,19 @@ void debugLinha(Expressao *lista, int tamLista)
     }
 }
 
+//Verifica se existe mais do que um positivo numa expressa, se sim retornar 1 senão retornar 0
+int verificarPositivos(Expressao *lista, int tamLista)
+{
+    for(int i = 0; i<tamLista; i++)
+    {
+        if(lista[i].tamPositivos > 1)
+        {
+            return 1;
+        }
+    }
+    return 0;
+}
+
 int main()
 {
     //Abre o ficheira
@@ -97,16 +139,22 @@ int main()
     //Este while irá ler linha a linha
     while((read = getline(&linha, &tam, f)) != -1)
     {
-        printf("%s\n", linha);
+        printf("\n\n====================================NOVA LINHA====================================\n%s\n", linha);
         tamLista = DesfragmentarLinha(linha,&lista); 
         debugLinha(lista, tamLista);
-        
-        /*for(int f = 0; f< lista[3].tamPositivos; f++)
-            {
-                printf("%c \n", lista[3].Positivos[f]);
-            }
-        printf("%d\n",lista[3].tamPositivos);*/
-    }
 
+        //Se por acaso alguma das expressoes da lista tiver mais do que um positivo deve printar NA e nem fazer mais nada
+        if(verificarPositivos(lista, tamLista) == 1)
+        {
+            printf("NA\n");
+            continue;
+        }
+
+        
+        
+        //Dá reset na lista e no seu tamanho para ser reutilizado
+        tamLista = reset(&lista);
+    }
+    free(lista);
     return 0;
 }
