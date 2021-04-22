4bbf376fca832d48a6c9a125150abe8d05580edf
angelomorgado <angelo_morgado@hotmail.com>
2021-04-22 20:48:14 +0100

    Bug fixes

--- main.c
+++ main.c
@@ -6,18 +6,19 @@
 
 typedef struct Expressao{
     char *Positivos;
+    int tamPositivos;
     char *Negativos;
+    int tamNegativos;
 }Expressao;
 
-//Versão simplificada da função do split que funciona
-//de forma eficaz para o problema em questão
-void DesfragmentarLinha(char * linha, Expressao *lista)
+//
+int DesfragmentarLinha(char *linha, Expressao **lista)
 {
     //indice que vamos utilizar para percorrer a lista
     int i=0;
 
     //Variáveis auxiliares de cada expressao
-    int tamLista=0, tamPositivos=0, tamNegativos=0;
+    int tamLista=0;
 
     while (linha[i] != '\0')
     {
@@ -25,71 +26,58 @@ void DesfragmentarLinha(char * linha, Expressao *lista)
         if(linha[i] == '(')
         {
             tamLista++;
-            lista = realloc(lista, tamLista * sizeof(Expressao));
-            //Inicializa a lista de inteiros ou negativos dessa expressão
-            lista[tamLista-1].Negativos = malloc(1);
-            lista[tamLista-1].Positivos = malloc(1);
-            tamPositivos=0; 
-            tamNegativos=0;
+            (*lista) = realloc((*lista), tamLista * sizeof(Expressao));
+            //Inicializa a (*lista) de inteiros ou negativos dessa expressão
+            (*lista)[tamLista-1].Negativos = malloc(1);
+            (*lista)[tamLista-1].Positivos = malloc(1);
+            (*lista)[tamLista-1].tamPositivos=0; 
+            (*lista)[tamLista-1].tamNegativos=0;
         }
 
         //Se encontrar um '~' significa que o próximo caracter é um literal negativo
         if(linha[i] == '~')
         {
-            //Atualiza a lista de negativos e adiciona o caracter negativo
-            tamNegativos++;
-            lista[tamLista-1].Negativos = realloc(lista[tamLista-1].Negativos, tamNegativos * sizeof(char));
-            lista[tamLista-1].Negativos[tamNegativos-1] = linha[i+1];
-            //Avança na lista para não repetir o caracter
+            //Atualiza a (*lista) de negativos e adiciona o caracter negativo
+            (*lista)[tamLista-1].tamNegativos++;
+            (*lista)[tamLista-1].Negativos = realloc((*lista)[tamLista-1].Negativos, (*lista)[tamLista-1].tamNegativos * sizeof(char));
+            (*lista)[tamLista-1].Negativos[(*lista)[tamLista-1].tamNegativos-1] = linha[i+1];
+            //Avança na (*lista) para não repetir o caracter
             i++;
         }
 
         //Se for uma letra, adicionar aos positivos
         if(((linha[i] >= 65 && linha[i] <=90) || (linha[i] >= 97 && linha[i] <=122)) && linha[i-1] != '~')
         {
-            //Atualiza a lista de positivos e adiciona o caracter positivo
-            tamPositivos++;
-            lista[tamLista-1].Positivos = realloc(lista[tamLista-1].Positivos, tamPositivos * sizeof(char));
-            lista[tamLista-1].Positivos[tamPositivos-1] = linha[i];
+            //Atualiza a (*lista) de positivos e adiciona o caracter positivo
+            (*lista)[tamLista-1].tamPositivos++;
+            (*lista)[tamLista-1].Positivos = realloc((*lista)[tamLista-1].Positivos, (*lista)[tamLista-1].tamPositivos * sizeof(char));
+            (*lista)[tamLista-1].Positivos[(*lista)[tamLista-1].tamPositivos-1] = linha[i];
         }
 
         //Avançar na linha
         i++;
     }
+    return tamLista;
+}
 
-
-    printf("\nPOSITIVOS DO ULTIMO\n");
-    for(int f=0;f<tamPositivos; f++)
+void debugLinha(Expressao *lista, int tamLista)
+{
+    for(int i=0; i< tamLista; i++)
     {
-        printf("%c ", lista[1].Positivos[f]);
-    }
-    printf("\n\nNEGATIVOS DO ULTIMO\n");
-    for(int f=0;f<21; f++)
-    {
-        printf("%c ", lista[1].Negativos[f]);
-    }
-
-    //Printa a linha como forma de debug, não apagar!
-    /*for(int i=0; i<tamLista;i++)
-    {
-        //Printa os positivos da expressao
-        printf("Positivos da expressao %d:\n", i);
-        int f=0;
-        while (lista[i].Positivos[f] != '\0')
+        printf("\n============================ EXPRESSAO %d =========================\n\n", i+1);
+        printf("Elementos positivos:\n");
+        for(int f = 0; f< lista[i].tamPositivos; f++)
         {
-            printf("%d ", lista[i].Positivos[f]);
+            printf("%c ", lista[i].Positivos[f]);
         }
         printf("\n\n");
-        //Printa os negativos da expressao
-        printf("Negativos da expressao %d:\n", i);
-        f=0;
-        while (lista[i].Negativos[f] != '\0')
+        printf("Elementos negativos:\n");
+        for(int f = 0; f< lista[i].tamNegativos; f++)
         {
-            printf("%d ", lista[i].Negativos[f]);
+            printf("%c ", lista[i].Negativos[f]);
         }
-
-        printf("\n\n\n");
-    }*/
+        printf("\n");
+    }
 }
 
 int main()
@@ -105,11 +93,19 @@ int main()
     char *linha = NULL;
     size_t read, tam;
     Expressao *lista = malloc(1);
+    int tamLista;
     //Este while irá ler linha a linha
     while((read = getline(&linha, &tam, f)) != -1)
     {
         printf("%s\n", linha);
-        DesfragmentarLinha(linha,lista);
+        tamLista = DesfragmentarLinha(linha,&lista); 
+        debugLinha(lista, tamLista);
+        
+        /*for(int f = 0; f< lista[3].tamPositivos; f++)
+            {
+                printf("%c \n", lista[3].Positivos[f]);
+            }
+        printf("%d\n",lista[3].tamPositivos);*/
     }
 
     return 0;
